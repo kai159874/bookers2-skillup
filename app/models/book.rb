ad21a0c43f96d89db8a9c1d396f87946047ffad5
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-04-02 09:48:05 +0000

    [add]notification

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,18 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :notifications, as: :notifiable, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
+  end
+  
+  after_create do
+    user.followers.each do |follower|
+      notifications.create(user_id: follower.id)
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     get "followers" => "relationships#followers", as: "followers"
   end
 
+  resources :notifications, only: [:update]
+
   get 'search' => "searches#search", as: "search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
